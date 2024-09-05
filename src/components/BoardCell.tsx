96f9be4865cb9f693e25dcc76404e628d212c802
Jakub Bröckl <brockljakub@gmail.com>
2024-09-05 22:07:44 +0200

    Change: BoardCell

---
+++ src/components/BoardCell.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { CellEnum, CellType } from "../common-types/Cell.d";
+
+export function BoardCell(props: {
+  x: number;
+  y: number;
+  cell: CellType;
+  onCellClick: (x: number, y: number) => void;
+}) {
+  const { x, y, cell, onCellClick } = props;
+
+  return (
+    <a
+      key={`${x};${y}`}
+      style={{
+        color: `${cell == CellEnum.X ? "green" : "red"}`,
+        width: "1.5rem",
+        height: "1.5rem",
+        border: "#571b1b solid 1px",
+        cursor: "pointer",
+      }}
+      id={`${x}|${y}`}
+      href={`#${x};${y}`}
+      onClick={() => {
+        // TODO: Disable pointer cursor
+        // TODO: move this click handling condition higher
+        if (cell == CellEnum.empty) {
+          onCellClick(x, y);
+        }
+      }}
+    >
+      {cell}
+    </a>
+  );
+}
--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Board } from "../utils/BoardValidator/Board";
 import { CellEnum } from "../common-types/Cell.d";
+import { BoardCell } from "./BoardCell";
 
 export function BoardComponent(props: {
   board: Board | null;
@@ -17,27 +18,7 @@ export function BoardComponent(props: {
             style={{ width: "100%", display: "flex", justifyContent: "center" }}
           >
             {row.map((cell, y) => (
-              <a
-                key={`${x};${y}`}
-                style={{
-                  color: `${cell == CellEnum.X ? "green" : "red"}`,
-                  width: "1.5rem",
-                  height: "1.5rem",
-                  border: "#571b1b solid 1px",
-                  cursor: "pointer",
-                }}
-                id={`${x}|${y}`}
-                href={`#${x};${y}`}
-                onClick={() => {
-                  // TODO: Disable pointer cursor
-                  // TODO: move this click handling condition higher
-                  if (cell == CellEnum.empty) {
-                    onCellClick(x, y);
-                  }
-                }}
-              >
-                {cell}
-              </a>
+              <BoardCell cell={cell} x={x} y={y} onCellClick={onCellClick} />
             ))}
           </div>
         ))}
